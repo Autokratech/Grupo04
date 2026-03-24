37c7b7eed3c45135fb782dceda619249cfab5d45
Reka <rgonzarod@gmail.com>
2026-03-24 17:38:22 +0100

    Dashboard refactor: split functions, added models and repository layer, improve dependency management

---
+++ backend/app/repositories/dashboard_repository.py
@@ -0,0 +1,54 @@
+from supabase import AsyncClient
+from app.core.exceptions import DatabaseError
+
+
+class DashboardRepository:
+    
+    #-- Tablas SQL de Supabase
+    DASHBOARD_TABLE = "dashboards"
+
+    def __init__(self, supabase: AsyncClient):
+        self.supabase = supabase
+
+
+    #-- Métodos para obtener el dashboard_id de un usuario concreto
+    async def get_user_dashboard(self, user_id : int):
+        try:
+            return await self.supabase.table(self.DASHBOARD_TABLE) \
+                .select('dashboard_id') \
+                .eq("user_id", user_id) \
+                .execute() 
+        except DatabaseError as e:
+            raise DatabaseError(e)
+
+
+    #-- Método para crear un dashboard nuevo
+    async def create_dashboard(self, user_id : int):
+        try:
+            return await self.supabase.table(self.DASHBOARD_TABLE) \
+                .insert({'user_id' : user_id}) \
+                .execute() 
+        except DatabaseError as e:
+            raise DatabaseError(e)
+
+
+    #-- Método para actualizar el theme del dashboard
+    async def update_dashboard_theme(self, dashboard_id : int, dashboard_theme : str):
+        try:
+            return await self.supabase.table(self.DASHBOARD_TABLE) \
+                .update({"dashboard_theme" : dashboard_theme}) \
+                .eq("dashboard_id", dashboard_id) \
+                .execute() 
+        except DatabaseError as e:
+            raise DatabaseError(e)
+
+
+    #-- Método para eliminar un dashboard
+    async def delete_dashboard(self, dashboard_id : int):
+        try:
+            return await self.supabase.table(self.DASHBOARD_TABLE) \
+                .delete() \
+                .eq("dashboard_id", dashboard_id) \
+                .execute() 
+        except DatabaseError as e:
+            raise DatabaseError(e)
