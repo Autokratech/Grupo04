793c45ead4678211b41391ad8d13b8d641f459c5
Sammy <svmmydev@gmail.com>
2026-04-26 16:28:06 +0200

    refactor: refactorize dashboard repository and the dashboard_viewmodel methods to adapt the backend

--- frontend/lib/data/repositories/dashboard_repository/dashboard_repository.dart
+++ frontend/lib/data/repositories/dashboard_repository/dashboard_repository.dart
@@ -1,4 +1,6 @@
+import 'package:frontend/domain/models/dashboard.dart';
 import 'package:frontend/domain/models/dashboard_preset.dart';
+import 'package:frontend/domain/models/dashboard_tab.dart';
 import 'package:frontend/domain/models/dashboard_widget_item.dart';
 
 abstract class DashboardRepository {
@@ -7,4 +9,9 @@ abstract class DashboardRepository {
   Future<List<DashboardWidgetItem>> getDashboardItems({
     required String presetId,
   });
+
+  Future<Dashboard> getDashboard();
+  Future<List<DashboardTab>> getDashboardTabs({required String dashboardId});
+  Future<List<DashboardWidgetItem>> getTabItems({required String tabId});
+  Future<DashboardTab> createDashboardTab({required String dashboardId, required String name});
 }
---
+++ frontend/lib/domain/models/dashboard_tab.dart
@@ -0,0 +1,11 @@
+class DashboardTab {
+  final String id;
+  final int position;
+  final String name;
+
+  const DashboardTab({
+    required this.id,
+    required this.position,
+    required this.name,
+  });
+}
