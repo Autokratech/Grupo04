4fdc3770b45b8ef0cba8ce7ddc02c5d159259180
Sammy <svmmydev@gmail.com>
2026-05-01 16:09:28 +0200

    chore: add dto models for dashboard backend response

---
+++ frontend/lib/data/models/dto/tab_widgets_response_dto.dart
@@ -0,0 +1,39 @@
+import 'package:frontend/data/models/dto/tab_widget_data_dto.dart';
+import 'package:frontend/data/models/dto/tab_widget_dto.dart';
+
+class TabWidgetsResponseDto {
+  final List<TabWidgetDto> tabWidgets;
+  final List<TabWidgetDataDto> tabWidgetsData;
+
+  const TabWidgetsResponseDto({
+    required this.tabWidgets,
+    required this.tabWidgetsData,
+  });
+
+  factory TabWidgetsResponseDto.fromMap(Map<String, dynamic> map) {
+    return TabWidgetsResponseDto(
+      tabWidgets: _parseList(
+        map['tab_widgets'],
+        TabWidgetDto.fromMap,
+      ),
+      tabWidgetsData: _parseList(
+        map['tab_widgets_data'],
+        TabWidgetDataDto.fromMap,
+      ),
+    );
+  }
+}
+
+List<T> _parseList<T>(
+    dynamic value,
+    T Function(Map<String, dynamic> map) fromMap,
+    ) {
+  if (value is! List) {
+    return [];
+  }
+
+  return value
+      .whereType<Map>()
+      .map((item) => fromMap(Map<String, dynamic>.from(item)))
+      .toList();
+}
